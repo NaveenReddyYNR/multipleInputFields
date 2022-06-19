a599792cef3e3b105674ccd7c7f78a639f7818e8
NaveenReddyYNR <100398391+NaveenReddyYNR@users.noreply.github.com>
2022-06-19 07:45:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,112 +1,108 @@
-import { useState } from 'react';
-import './App.css';
+import React from "react";
 
-function App() {
-  const [persons, setPersons] = useState([
-    { name: '', age: '', phoneNumbers:[] }
-  ])
+function Persons(props) {
+  const [persons, setPersons] = React.useState([
+    {
+      name: "",
+      age: "",
+      phoneNumbers: [""],
+    },
+  ]);
 
   const handleFormChange = (event, index) => {
-    let data = [...persons];
+    let data = JSON.parse(JSON.stringify(persons));
     data[index][event.target.name] = event.target.value;
     setPersons(data);
-  }
-
-  const submit = (e) => {
-    e.preventDefault();
-    console.log(persons)
-  }
+  };
 
   const addPersonFields = () => {
     let person = {
-      name: '',
-      age: '',
-      phoneNumbers:[ ]
-    }
-    setPersons([...persons, person])
-  }
+      name: "",
+      age: "",
+      phoneNumbers: [""],
+    };
+    setPersons([...persons, person]);
+  };
+
+  const updatePhoneNumber = (event, phoneIndex, personIndex) => {
+    let data = JSON.parse(JSON.stringify(persons));
+    let person = data[personIndex];
+    let phoneNumbers = person.phoneNumbers;
+    phoneNumbers[phoneIndex] = event.target.value;
+    setPersons(data);
+  };
 
   const addPhoneNumberField = (personIndex) => {
-    //console.log("persons", persons);
-    let personsLocal = [...persons];
-    
+    let data = JSON.parse(JSON.stringify(persons));
+    let person = data[personIndex];
+    let phoneNumbers = person.phoneNumbers;
+    phoneNumbers.push("");
+    setPersons(data);
+  };
 
-   personsLocal[personIndex]["phoneNumbers"].push("");
-   //console.log("phoneNumbers", personsLocal[personIndex]["phoneNumbers"]);
-   //console.log("persons", persons);
-  //  this.setState(prevState => {
-  //   const team = [...prevState.team];
-  //   team[index] = { ...team[index], [name]: value };
-  //   return { team };
-  // });
-   setPersons(prevState => {
-    const personsDuplicate = [...persons];
-    personsDuplicate[personIndex] = {...personsDuplicate[personIndex], [....personsDuplicate[personIndex].phoneNumbers]:[...personsDuplicate[personIndex].phoneNumbers, ""]};
-   });
-   console.log("persons", persons);
-    //onChange={(e)=> props.updateCuration({...props.curation, titles:[...props.curation.titles, e.target.value] }) }
-  }
-
-  const removeFields = (index) => {
-    let data = [...persons];
-    data.splice(index, 1)
-    setPersons(data)
-  }
-  const personsLength = persons.length;
   return (
-    <div className="App">
-      <form onSubmit={submit}>
-        {persons.map((person, personIndex) => {
-          return (
-            <center>
-            <div key={personIndex} style={{width:"200px", display:"block",border:"1px solid gray"}}>
+    <div>
+      {persons?.map((person, personIndex) => {
+        return (
+          <div
+            key={personIndex}
+            style={{
+              width: 210,
+              display: "block",
+              border: "1px solid black",
+              marginBottom: 48,
+              marginLeft: 25,
+            }}
+          >
+            <div style={{ display: "flex", marginBottom: 16 }}>
               <input
-                style={{border:"none",height:"50%",width:"80%"}}
-                name='name'
-                placeholder='Name'
-                onChange={event => handleFormChange(event, personIndex)}
+                style={{ height: 36, width: 200, border: "1px solid gray" }}
+                name="name"
+                placeholder="Name"
                 value={person.name}
+                onChange={(event) => handleFormChange(event, personIndex)}
               />
-              <span>
-              <i className="AddField" onClick={addPersonFields}>+</i>
-              <RemoveButton arrLength={personsLength} removeFields={() => removeFields(personIndex)} />
+              <span style={{ padding: 10, fontSize: 20, cursor: "pointer" }}>
+                <i onClick={addPersonFields}> + </i>
               </span>
-              <input
-                style={{border:"none"}}
-                name='age'
-                placeholder='Age'
-                onChange={event => handleFormChange(event, personIndex)}
-                value={person.age}
-              />
-              <input
-                style={{border:"none"}}
-                name='phoneNumbers'
-                placeholder='Phone Number'
-                // onChange={event => updatePhoneNumber(event, personIndex)}
-                value={person.phoneNumbers}
-              /><span> <i className="AddField" onClick={() => addPhoneNumberField(personIndex)}>+</i></span>
-              </div>
-              <button ><i className="AddField" onClick={() => removeFields(personIndex)}>Remove</i></button>
-            </center>
-          )
-        })}
-      </form>
-      <center>
-          {/* <button onClick={addFields}>Add More..</button> */}
-          <br />
-          <button onClick={submit}>Submit</button>
-      </center>
+            </div>
+            <input
+              style={{
+                height: 36,
+                width: 200,
+                border: "1px solid gray",
+                marginBottom: 16,
+              }}
+              name="age"
+              placeholder="Age"
+              value={person.age}
+              onChange={(event) => handleFormChange(event, personIndex)}
+            />
+            {person.phoneNumbers?.map((phone, phoneIndex) => {
+              return (
+                <div style={{ display: "flex", marginBottom: 16 }}>
+                  <input
+                    style={{ height: 36, width: 200, border: "1px solid gray" }}
+                    name="phoneNumbers"
+                    value={phone}
+                    placeholder="Phone Number"
+                    onChange={(event) =>
+                      updatePhoneNumber(event, phoneIndex, personIndex)
+                    }
+                  />
+                  <span
+                    style={{ padding: 10, fontSize: 20, cursor: "pointer" }}
+                  >
+                    <i onClick={() => addPhoneNumberField(personIndex)}> + </i>
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
 
-function RemoveButton(props){
-  if(props.arrLength == 1) {return (<></>)}
-  else {
-  return (
-    <i className="AddField" onClick={props.removeFields}>-</i>
-  );
-  }
-}
-
-export default App;
+export default Persons;
